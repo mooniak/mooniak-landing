395cfafc32179535c0235a9475888defa4dfb9de
Pathum Egodawatta <pathumego@gmail.com>
2015-04-23 14:39:53 +0530

    Updates

--- update.sh
+++ update.sh
@@ -1,6 +1,9 @@
 cd;
 cd src/github.com/mooniak/mooniak-web/;
-git submodule foreach git pull;
+git pull;
 git add --all;
 git commit -m'Updates';
+git submodule foreach git pull;
+git add --all;
+git commit -m'Update Submodules';
 git push
